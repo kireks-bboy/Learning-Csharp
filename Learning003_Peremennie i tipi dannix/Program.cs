6c81fb44b270933453b98aa8570eecd675d275d2
Kirill Denisevitch <denviko00@gmail.com>
2022-11-23 00:59:34 +0300

    PRACTICA s peremennimi

--- Learning003_Peremennie i tipi dannix/Program.cs
+++ Learning003_Peremennie i tipi dannix/Program.cs
@@ -4,18 +4,33 @@ namespace Learning003 {
 
   class Program {
     static void Main() {
+
+int num_1 = 0, num_2 = 0, num_3 = 0;
+num_1 = Convert.ToInt32(Console.ReadLine());
+num_2 = Convert.ToInt32(Console.ReadLine());
+num_3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("First: " + num_1 + ". Second: " + num_2 + ". Three: " + num_3);
+
+
+
+
+
+
+
+
       // uint number = 10; //Только положительные числа uint
-int number = -10;
+// int number = -10;
 // byte number = 255; //Диапозон чисел до 255
 // short number = 32000; // до 32000
 // long number = 900; //
 // float num = 4.5345f;
 // double num2 = 5.324d;
-string word = "Peremennaya: ";
+// string word = "Peremennaya: ";
 // char symbol = 's';
 // bool isHappy = True;
 
-Console.WriteLine(word + number + ".");
+// Console.WriteLine(word + number + ".");
 // Console.WriteLine("Peremennaya: " + number + ".");
 
       // Получение данных
