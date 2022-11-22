0d343a9170d74d9712e4b99ea96add999da33fa1
Kirill Denisevitch <denviko00@gmail.com>
2022-11-23 00:49:01 +0300

    LS3 Peremennie commands

--- Learning003_Peremennie i tipi dannix/Program.cs
+++ Learning003_Peremennie i tipi dannix/Program.cs
@@ -4,9 +4,19 @@ namespace Learning003 {
 
   class Program {
     static void Main() {
+      // uint number = 10; //Только положительные числа uint
 int number = -10;
+// byte number = 255; //Диапозон чисел до 255
+// short number = 32000; // до 32000
+// long number = 900; //
+// float num = 4.5345f;
+// double num2 = 5.324d;
+string word = "Peremennaya: ";
+// char symbol = 's';
+// bool isHappy = True;
 
-Console.WriteLine("Peremennaya: " + number + ".");
+Console.WriteLine(word + number + ".");
+// Console.WriteLine("Peremennaya: " + number + ".");
 
       // Получение данных
       //Console.ReadKey();
