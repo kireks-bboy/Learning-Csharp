3bcb86194502e6a24a373451add9a49eef6b70bd
Kirill Denisevitch <denviko00@gmail.com>
2022-11-23 00:01:15 +0300

    WriteLine

---
+++ Learning002/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Learning002 {
+
+  class Program {
+    static void Main() {
+      Console.WriteLine("Hello 1");
+      Console.WriteLine("Hello 2");
+      Console.WriteLine("Hello 3");
+        
+      Console.ReadKey();
+    }
+  }
+}
