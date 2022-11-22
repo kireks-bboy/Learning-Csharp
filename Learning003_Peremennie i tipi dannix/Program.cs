02e5ebd99e4d2fde542874ad004fd98d9a942b7d
Kirill Denisevitch <denviko00@gmail.com>
2022-11-23 00:25:55 +0300

    Text plus vivod dannix

---
+++ Learning003_Peremennie i tipi dannix/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Learning003 {
+
+  class Program {
+    static void Main() {
+int number = -10;
+
+Console.WriteLine("Peremennaya: " + number);
+
+      // Получение данных
+      //Console.ReadKey();
+      
+    }
+  }
+}
