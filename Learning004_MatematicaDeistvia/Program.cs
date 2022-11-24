fde890e244076b192e04c2bf1187ee620348370a
Kirill Denisevitch <denviko00@gmail.com>
2022-11-24 22:49:03 +0300

    Matematica deystvia_slozenie_ymnoz_vich_delenie_ostatok

---
+++ Learning004_MatematicaDeistvia/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Learning003 {
+
+  class Program {
+    static void Main() {
+
+Console.Write("Vvedite chislo s tochkoy: ");
+// user_input = Console.ToDouble(Console.ReadLine());
+float user_input = float.Parse(Console.ReadLine());
+
+float result;
+// result = user_input + 10f;
+// result = user_input - 15f;
+// result = user_input * 2f;
+// result = user_input / 5f;
+result = user_input % 2f;
+
+Console.WriteLine("Peremennaya: " + result);
+
+// Console.WriteLine("Peremennaya: " + user_input);
+
+
+
+
+
+
+
+      
+    }
+  }
+}
