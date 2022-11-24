4dfa837bbbdc74b693aa33ac52af716c275ed4b2
Kirill Denisevitch <denviko00@gmail.com>
2022-11-24 23:12:43 +0300

    Matematica_Functions

--- Learning004_MatematicaDeistvia/Program.cs
+++ Learning004_MatematicaDeistvia/Program.cs
@@ -5,28 +5,52 @@ namespace Learning003 {
   class Program {
     static void Main() {
 
-Console.Write("Vvedite chislo s tochkoy: ");
-// user_input = Console.ToDouble(Console.ReadLine());
-float user_input = float.Parse(Console.ReadLine());
 
-float result;
-// result = user_input + 10f;
-// result = user_input - 15f;
-// result = user_input * 2f;
-// result = user_input / 5f;
-result = user_input % 2f;
-
-result = result + 5f; // Если хотим к result добавить еще + 5, сокращенная запись: result += 5f , так же можно +/*%;
-result++; // Добавляем еще +1 , а если отнять то result--;
-
-// int a = 5, b = 6;
-// int res = a + b;
+// System.Console.WriteLine(Math.Abs(-20));
+// System.Console.WriteLine(Math.Ceiling(4.56));
+// System.Console.WriteLine(Math.Floor(4.57));
+// System.Console.WriteLine(Math.Round(4.49));
+// System.Console.WriteLine(Math.Min(4 , 2));
+// System.Console.WriteLine(Math.Max(4 , 2));
+System.Console.WriteLine(Math.Pow(4 , 3));
 
 
 
-Console.WriteLine("Peremennaya: " + result);
 
-// Console.WriteLine("Peremennaya: " + user_input);
+
+
+
+
+
+
+
+
+
+
+
+
+// Console.Write("Vvedite chislo s tochkoy: ");
+// // user_input = Console.ToDouble(Console.ReadLine());
+// float user_input = float.Parse(Console.ReadLine());
+
+// float result;
+// // result = user_input + 10f;
+// // result = user_input - 15f;
+// // result = user_input * 2f;
+// // result = user_input / 5f;
+// result = user_input % 2f;
+
+// result = result + 5f; // Если хотим к result добавить еще + 5, сокращенная запись: result += 5f , так же можно +/*%;
+// result++; // Добавляем еще +1 , а если отнять то result--;
+
+// // int a = 5, b = 6;
+// // int res = a + b;
+
+
+
+// Console.WriteLine("Peremennaya: " + result);
+
+// // Console.WriteLine("Peremennaya: " + user_input);
 
 
 
