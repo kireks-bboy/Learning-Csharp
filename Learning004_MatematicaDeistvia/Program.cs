2b38bbfd84f3d638c4a4632bb59b1b34780f27ba
Kirill Denisevitch <denviko00@gmail.com>
2022-11-24 22:56:32 +0300

    Matematica_02

--- Learning004_MatematicaDeistvia/Program.cs
+++ Learning004_MatematicaDeistvia/Program.cs
@@ -16,6 +16,14 @@ float result;
 // result = user_input / 5f;
 result = user_input % 2f;
 
+result = result + 5f; // Если хотим к result добавить еще + 5, сокращенная запись: result += 5f , так же можно +/*%;
+result++; // Добавляем еще +1 , а если отнять то result--;
+
+// int a = 5, b = 6;
+// int res = a + b;
+
+
+
 Console.WriteLine("Peremennaya: " + result);
 
 // Console.WriteLine("Peremennaya: " + user_input);
