a27608643289001d5bf9e436cfd3ae535603c8a2
Kirill Denisevitch <denviko00@gmail.com>
2022-11-26 23:21:07 +0300

    if_else_ifelse

---
+++ Learning005_If_Else/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Learning003 {
+
+  class Program {
+    static void Main() {
+
+int a = 5;
+
+// bool isHappy = true;
+// if (isHappy == false) {
+
+// }
+
+if (a > 7) {
+  System.Console.WriteLine("Number > 7");
+  
+} else if (a < 5) {
+  System.Console.WriteLine("Number is < 5");
+}
+
+else{
+  System.Console.WriteLine("Number is unrecognized");
+}
+
+    }
+  }
+}
