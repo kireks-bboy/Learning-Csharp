ad9781652f06a11bad79d2611842a139c12a3818
Kirill Denisevitch <denviko00@gmail.com>
2022-11-26 23:34:43 +0300

    neskol'ko_ysloviy

--- Learning005_If_Else/Program.cs
+++ Learning005_If_Else/Program.cs
@@ -6,6 +6,7 @@ namespace Learning003 {
     static void Main() {
 
 int a = 5;
+bool isHasCar = true;
 
 // bool isHappy = true;
 // if (isHappy == false) {
@@ -17,6 +18,12 @@ if (a > 7) {
   
 } else if (a < 5) {
   System.Console.WriteLine("Number is < 5");
+  } else if (a == 4) {
+  System.Console.WriteLine("Number is 4");
+  } else if ((a == 5 || isHasCar) && a > 2 ) {
+  System.Console.WriteLine("Number is 5");
+// } if (isHasCar) {
+  System.Console.WriteLine("User has car!");
 }
 
 else{
