293c9ee48ce2ff517cd2c30ba68c200f19afccc2
Kirill Denisevitch <denviko00@gmail.com>
2022-11-28 21:55:23 +0300

    break

--- Learning007_ForWhileDowhile/Program.cs
+++ Learning007_ForWhileDowhile/Program.cs
@@ -1,32 +1,49 @@
 ﻿using System;
 
-namespace Learning007 {
+namespace Learning007
+{
 
-  class Program {
-    static void Main() {
-      
+  class Program
+  {
+    static void Main()
+    {
+
       // for(float i = 200; i > 10; i /=2) 
       //   System.Console.WriteLine("Element: {0}", i);
 
-// byte i = 10;
-//       while(i >= 1) {
-//         System.Console.WriteLine("Element: {0}", i);
-//         i--;
+      // byte i = 10;
+      //       while(i >= 1) {
+      //         System.Console.WriteLine("Element: {0}", i);
+      //         i--;
 
-//       }
-//       bool isHasCar = true;
-//       while(isHasCar) {
-//         string end = System.Console.ReadLine();
-//         if(end == "end")
-//         isHasCar = false;
-//       }
+      //       }
+      //       bool isHasCar = true;
+      //       while(isHasCar) {
+      //         string end = System.Console.ReadLine();
+      //         if(end == "end")
+      //         isHasCar = false;
+      //       }
 
-byte i = 100;
 
-do {
-System.Console.WriteLine("Element: " + i);
-} while (i < 10);
+
+
+      // byte i = 100;
+
+      // do {
+      // System.Console.WriteLine("Element: " + i);
+      // } while (i < 10);
+
+
+
+
+
+      for (short i = 0; i < 10; i++)
+      {
+        if (i > 5)
+          break;
+        System.Console.WriteLine("El: " + i);
       }
-      
     }
+
   }
+}
