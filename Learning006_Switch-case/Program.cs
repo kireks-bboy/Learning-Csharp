afabfe3af93e92e2feab73294629fa1d1abd1d8c
Kirill Denisevitch <denviko00@gmail.com>
2022-11-28 21:16:55 +0300

    switch_case

---
+++ Learning006_Switch-case/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Learning006
+{
+
+  class Program
+  {
+    static void Main()
+    {
+
+      short user_input = Convert.ToInt16(Console.ReadLine());
+
+      switch(user_input) {
+        case 5:
+          Console.WriteLine("Number is 5");
+          break;
+           case 15:
+          Console.WriteLine("Number is 15");
+          break;
+           case 20:
+          Console.WriteLine("Number is 20");
+          break;
+           case 250:
+          Console.WriteLine("Number is 250");
+          break;
+          default:
+          Console.WriteLine("Number is unricognized!");
+         break;
+         
+      }
+
+            
+                
+    }
+  }
+}
