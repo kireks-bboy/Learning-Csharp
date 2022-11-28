dc58e2a9b2313641627c70883939687643594db2
Kirill Denisevitch <denviko00@gmail.com>
2022-11-28 21:44:11 +0300

    while

--- Learning007_ForWhileDowhile/Program.cs
+++ Learning007_ForWhileDowhile/Program.cs
@@ -5,10 +5,22 @@ namespace Learning007 {
   class Program {
     static void Main() {
       
-      for(float i = 200; i > 10; i /=2) {
+      // for(float i = 200; i > 10; i /=2) 
+      //   System.Console.WriteLine("Element: {0}", i);
+
+byte i = 10;
+      while(i >= 1) {
         System.Console.WriteLine("Element: {0}", i);
+        i--;
+
+      }
+      bool isHasCar = true;
+      while(isHasCar) {
+        string end = System.Console.ReadLine();
+        if(end == "end")
+        isHasCar = false;
+      }
       }
       
     }
   }
-}
