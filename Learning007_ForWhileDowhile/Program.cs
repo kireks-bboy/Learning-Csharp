9708d4f38e92215df73224eadf32f08a6a7a8289
Kirill Denisevitch <denviko00@gmail.com>
2022-11-28 21:50:56 +0300

    while_do

--- Learning007_ForWhileDowhile/Program.cs
+++ Learning007_ForWhileDowhile/Program.cs
@@ -8,18 +8,24 @@ namespace Learning007 {
       // for(float i = 200; i > 10; i /=2) 
       //   System.Console.WriteLine("Element: {0}", i);
 
-byte i = 10;
-      while(i >= 1) {
-        System.Console.WriteLine("Element: {0}", i);
-        i--;
+// byte i = 10;
+//       while(i >= 1) {
+//         System.Console.WriteLine("Element: {0}", i);
+//         i--;
 
-      }
-      bool isHasCar = true;
-      while(isHasCar) {
-        string end = System.Console.ReadLine();
-        if(end == "end")
-        isHasCar = false;
-      }
+//       }
+//       bool isHasCar = true;
+//       while(isHasCar) {
+//         string end = System.Console.ReadLine();
+//         if(end == "end")
+//         isHasCar = false;
+//       }
+
+byte i = 100;
+
+do {
+System.Console.WriteLine("Element: " + i);
+} while (i < 10);
       }
       
     }
