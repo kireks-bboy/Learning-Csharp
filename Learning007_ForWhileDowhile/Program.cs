cf11570debb155c1fbb52c355a7c27fdca48fcc8
Kirill Denisevitch <denviko00@gmail.com>
2022-11-28 21:29:42 +0300

    for

---
+++ Learning007_ForWhileDowhile/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Learning007 {
+
+  class Program {
+    static void Main() {
+      
+      for(float i = 200; i > 10; i /=2) {
+        System.Console.WriteLine("Element: {0}", i);
+      }
+      
+    }
+  }
+}
