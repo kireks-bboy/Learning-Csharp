fb57625d0a54ad7a7523e9dcece571947621ddc9
Kirill Denisevitch <denviko00@gmail.com>
2022-12-01 22:25:00 +0300

    massiv1

---
+++ Learning008_Massivs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Learning008 {
+
+  class Program {
+    static void Main() {
+     
+byte[] nums = new byte[5];
+nums[0] = 250;
+nums[1] = 50;
+nums[2] = 20;
+nums[3] = 100;
+nums[4] = 25;
+
+// System.Console.WriteLine("El: " + nums[0]);
+
+string[] words = new string[] {"John", "Bob" , "Alex"};
+
+words[1] = "Josh";
+
+for(byte i = 0; i < words.Length; i++ )
+System.Console.WriteLine("El: " + words[i]);
+    }
+  }
+}
