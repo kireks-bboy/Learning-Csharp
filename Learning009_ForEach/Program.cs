7632604352c8297ba14bb454c743fd9ce9052f03
Kirill Denisevitch <denviko00@gmail.com>
2022-12-02 23:25:51 +0300

    end

--- Learning009_ForEach/Program.cs
+++ Learning009_ForEach/Program.cs
@@ -1,10 +1,54 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Learning009 {
 
   class Program {
     static void Main() {
-     
+
+List<int> numbers = new List<int> () {
+4,6,7
+};
+
+numbers.Add(40);
+numbers.Add(100);
+numbers.Add(5);
+
+
+numbers.Remove(100);
+numbers.Sort();
+numbers.Reverse();
+
+
+foreach(int el in numbers) {
+  System.Console.WriteLine("El: " + el);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//      short[,] nums = {
+// {4,5,7},
+// {43,52,17},
+// {24,53,37},
+//      };
+
+//      foreach(short el in nums) {
+//       System.Console.WriteLine("El:" + el);
+//      }
       
     }
   }
