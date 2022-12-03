37e1f5e83ecc094f0879687afecc3a5f9dc9d5cb
Kirill Denisevitch <denviko00@gmail.com>
2022-12-03 21:43:44 +0300

    testtt

---
+++ test/Program.cs
@@ -0,0 +1,40 @@
+﻿// const double PI = 3.14; 
+
+// int a = 5+5;
+// int b = 5*5;
+// int c = 5/5;
+
+// string something = "Hello bro";
+// string str1 = "Hello";
+// string str2 = "Goodbye!";
+// string str3 = str1 + str2;
+
+// string x = null;
+// string x2 = x + "some";
+
+// int ac = 15;
+// sbyte bc = (sbyte)ac;
+
+// string str = "1000";
+// short xx = Convert.ToInt16(str);
+
+
+
+
+
+// System.Console.WriteLine(bc);
+// System.Console.WriteLine(str3);
+// System.Console.WriteLine(something);
+// System.Console.WriteLine(a);
+// System.Console.WriteLine(b);
+// System.Console.WriteLine(c);
+// System.Console.WriteLine(PI);
+// System.Console.WriteLine(x2);
+// System.Console.WriteLine(xx);
+
+
+System.Console.WriteLine("Введи число:");
+string x = Console.ReadLine();
+int a = Convert.ToInt32(x);
+a = a * a;
+System.Console.WriteLine(a);
